d34de000dfbc582cb4ce9d75d4903d515bb0ddba
Wkerlyson <wkerlyson.batista@gmail.com>
2019-10-14 20:55:40 -0300

    creating Hero model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:provider_app/home.dart';
 
 void main() => runApp(MyApp());
@@ -7,12 +8,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeWidget(),
       ),
-      home: HomeWidget(),
     );
   }
 }
